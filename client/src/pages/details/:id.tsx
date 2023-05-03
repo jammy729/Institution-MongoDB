dd7074cf736439a2efdf5454400e1787877f311a
James Yoo <53548018+jammy729@users.noreply.github.com>
2023-05-02 17:32:23 -0700

    Fix

--- client/src/pages/details/:id.tsx
+++ client/src/pages/details/:id.tsx
@@ -32,7 +32,7 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:9001/institution/details/${id}`)
+      .get(`https://institution-api.James yoo.dev/details/${id}`)
       .then((response) => {
         console.log(response.data);
         setInstitutionList(response.data);
